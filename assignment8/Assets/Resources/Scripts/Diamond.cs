897cf6127f3e5cc3d30a1e9600852477ff74fca7
Prajjwal Juyal <prajjwaljuyal@gmail.com>
2020-07-21 19:31:13 +0530

    Finish assignment8

---
+++ assignment8/Assets/Resources/Scripts/Diamond.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamond : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -25) {
+            Destroy(gameObject);
+        }
+        else {
+            transform.Translate(-SkyscraperSpawner.speed * Time.deltaTime, 0, 0, Space.World);
+        }
+
+        transform.Rotate(0, 3f, 0, Space.World);
+    }
+
+    void OnTriggerEnter(Collider other){
+
+        other.transform.parent.GetComponent<HeliController>().PickupDiamond();
+        Destroy(gameObject);
+    }
+}
---
+++ assignment8/Assets/Resources/Scripts/DiamondSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiamondSpawner : MonoBehaviour
+{
+
+    public GameObject[] prefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnDiamonds());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnDiamonds() {
+        while(true) {
+            Instantiate(prefabs[Random.Range(0, prefabs.Length)],
+            new Vector3(26, Random.Range(-10, 10), 10),
+            Quaternion.identity);
+
+            yield return new WaitForSeconds(Random.Range(1, 8));
+        }
+    }
+}
